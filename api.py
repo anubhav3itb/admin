a4b5df8b2115581438e1e524dc898cf0b2f5f764
Anubhav <bharanu@hilti.com>
2017-09-13 02:43:26 +0800

    first commit

---
+++ api.py
@@ -0,0 +1,22 @@
+from flask import sessions, jsonify, Flask, render_template
+import requests
+import os
+import ast
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    data = requests.get('http://128.199.240.195:5000/getAllIntentReport').content
+    data = ast.literal_eval(data)
+    x = zip(data['user_name'], data['user_query'], data['user_intent'], data['reported_intent'], data['link'])
+    return render_template('index.html',
+                            title='Intent Reports',
+                            x=x)
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
